0d6880e1bd5e28bea284cacda748ccc676230c1f
jay <jay@code.com>
2022-09-08 12:34:42 +0800

    third commit

--- src/main/java/com/lp/git/GitTest.java
+++ src/main/java/com/lp/git/GitTest.java
@@ -2,8 +2,9 @@ package com.lp.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,Git!");
         System.out.println("hello,Git1!");
         System.out.println("hello,Git2!");
+        System.out.println("hello,Git3!");
+        System.out.println("hello,Git4!");
     }
 }
